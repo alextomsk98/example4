e2a3229f406bd999f3b40531da02498edbe41a54
alextomsk98 <shavlo.2016@mail.ru>
2018-02-05 18:15:17 +0700

    laba1(4)

---
+++ example4/example4/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example4
+{
+    class Car
+    {
+        public string petName;
+        public int currSpeed;
+        public Car()
+        {
+            petName = "Chuck";
+            currSpeed = 10;
+        }
+        public Car(string pn)
+        {
+            petName = pn;
+        }
+        public Car(string pn, int cs)
+        {
+            petName = pn;
+            currSpeed = cs;
+        }
+    }
+}
---
+++ example4/example4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Fun with Class Types*****\n");
+            Car chuck = new Car();
+
+            Car mary = new Car("Mary");
+
+            Car daisy = new Car("Daisy", 75);
+            Console.ReadLine();
+        }
+    }
+}
